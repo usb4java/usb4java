45aea8d9bc6bb892f95999255dc71ebf2c5d31a9
Klaus Reimer <k@ailis.de>
2011-01-29 09:50:42 +0100

    Removed buid.xml and added shell scripts instead.

---
+++ src/main/c/build/linux-x86_64.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Builds libusb4java for 64 bit linux.
+# Must be executed on 64 bit linux machine and must have libusb-dev installed.
+
+set -e
+cd $(dirname $0)/..
+
+OS=linux
+ARCH=x86_64
+TMPDIR=$(pwd)/tmp
+DISTDIR=$(pwd)/../assembly/${OS}-${ARCH}
+
+# Clean up
+rm -rf $TMPDIR
+rm -rf $DISTDIR
+
+# Build autoconf stuff if needed
+if [ ! -e configure ]
+then
+    make -f Makefile.scm
+fi
+
+# Build libusb4java
+./configure --prefix=/ CFLAGS="-m64"
+make clean install-strip DESTDIR=$TMPDIR
+mkdir -p $DISTDIR
+cp -faL $TMPDIR/lib/libusb4java.so $DISTDIR/
+rm -rf $TMPDIR
