3e00d5b49832359ceafd9b5c93de18ec45201905
Klaus Reimer <k@ailis.de>
2013-04-16 22:13:19 +0200

    Add wrapper for memcpy for linux x86_64 build so the library can be build on a new linux machine without enforcing a dependency on GLIBC 2.14

--- src/main/c/build/linux-x86_64.sh
+++ src/main/c/build/linux-x86_64.sh
@@ -9,8 +9,9 @@ set -e
 OS="linux"
 ARCH="x86_64"
 HOST="$ARCH-$OS-gnu"
-CFLAGS="-m64"
+CFLAGS="-m64 -Wl,--wrap=memcpy"
 LIBUSB_CONFIG="--disable-shared"
 USB4JAVA_LIBS="-lrt"
+USB4JAVA_CFLAGS="-DWRAP_MEMCPY"
 
 build
