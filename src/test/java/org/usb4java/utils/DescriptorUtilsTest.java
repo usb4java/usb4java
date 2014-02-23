a81ce1e517f6b4d9595f371dc476e3afeb0f87bf
Klaus Reimer <k@ailis.de>
2014-02-23 13:14:07 +0100

    Make unit test platform-independent

--- src/test/java/org/usb4java/utils/DescriptorUtilsTest.java
+++ src/test/java/org/usb4java/utils/DescriptorUtilsTest.java
@@ -125,8 +125,8 @@ public class DescriptorUtilsTest
         bytes = ByteBuffer.allocate(20);
         bytes.put(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
             15, 16, 17, 18, 19, 20 });
-        assertEquals("01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f 10\n" +
-            "11 12 13 14", DescriptorUtils.dump(bytes));
+        assertEquals(String.format("01 02 03 04 05 06 07 08 09 0a 0b 0c 0d " +
+            "0e 0f 10%n11 12 13 14"), DescriptorUtils.dump(bytes));
     }
 
     /**
